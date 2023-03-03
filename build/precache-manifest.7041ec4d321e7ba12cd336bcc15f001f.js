44567119913fc591dbc303d700416e0c85f7d4c5
Mohammed Yusuf Abdul Lateef <89645307+yusuf3919@users.noreply.github.com>
2023-03-04 01:04:52 +0530

    essentila files

---
+++ build/precache-manifest.7041ec4d321e7ba12cd336bcc15f001f.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac76edf0c9e13c5d08ff",
+    "url": "/static/css/main.cb3af790.chunk.css"
+  },
+  {
+    "revision": "ac76edf0c9e13c5d08ff",
+    "url": "/static/js/main.ac76edf0.chunk.js"
+  },
+  {
+    "revision": "0367dfd9c84259b24175",
+    "url": "/static/js/1.0367dfd9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "87e2f920c829469bd082173360fd5150",
+    "url": "/static/media/Logo.87e2f920.jpg"
+  },
+  {
+    "revision": "d68cbec23d3b125b156e20165ecb9195",
+    "url": "/static/media/0.d68cbec2.png"
+  },
+  {
+    "revision": "b400d6991054ff0eda30098793c14991",
+    "url": "/static/media/1.b400d699.jpg"
+  },
+  {
+    "revision": "d102ffe21d2c305f9a7ed2b24d407d30",
+    "url": "/static/media/2.d102ffe2.jpg"
+  },
+  {
+    "revision": "dad23b14246c948b5cbd5665c7bb2e98",
+    "url": "/static/media/3.dad23b14.jpg"
+  },
+  {
+    "revision": "1acbec7d95fe95f7fb7a97231764c476",
+    "url": "/static/media/4.1acbec7d.jpg"
+  },
+  {
+    "revision": "3e4a0062e5c8f1697fba1dd293366895",
+    "url": "/static/media/5.3e4a0062.jpg"
+  },
+  {
+    "revision": "d7cd823c83f803f8ee31da2bd1253533",
+    "url": "/static/media/6.d7cd823c.jpg"
+  },
+  {
+    "revision": "301ec0cc7a06802358da594a3f1486f2",
+    "url": "/static/media/7.301ec0cc.jpg"
+  },
+  {
+    "revision": "68f6e2335b2274e062bc90403331ea57",
+    "url": "/static/media/8.68f6e233.jpg"
+  },
+  {
+    "revision": "33cf215586ab44b5a08d069efde91ce2",
+    "url": "/static/media/9.33cf2155.jpg"
+  },
+  {
+    "revision": "cecafa18b08d7a80ab87d059e60a97b3",
+    "url": "/static/media/10.cecafa18.jpg"
+  },
+  {
+    "revision": "0b271e1c4552ba960b6c38454bfd268c",
+    "url": "/static/media/11.0b271e1c.jpg"
+  },
+  {
+    "revision": "4cc6da513896fc90908698a6a93e22fc",
+    "url": "/static/media/12.4cc6da51.jpg"
+  },
+  {
+    "revision": "b5458296e086db9faade4cb2b933c3c2",
+    "url": "/static/media/13.b5458296.jpg"
+  },
+  {
+    "revision": "7022487d9081212d2ec72da6d1a37e48",
+    "url": "/static/media/14.7022487d.jpg"
+  },
+  {
+    "revision": "0b48fad87231606d2f681da569227ba4",
+    "url": "/static/media/15.0b48fad8.jpg"
+  },
+  {
+    "revision": "ac88c0722ac3ea6619e22d1228c8bfa6",
+    "url": "/static/media/16.ac88c072.jpg"
+  },
+  {
+    "revision": "289c86a0178226a0a2d0bac8e2906f3a",
+    "url": "/static/media/17.289c86a0.jpg"
+  },
+  {
+    "revision": "9bd2096f162ebb19250b432cf98df5dc",
+    "url": "/static/media/18.9bd2096f.jpg"
+  },
+  {
+    "revision": "f100ab5e60cf02cac7cf057a79946539",
+    "url": "/static/media/19.f100ab5e.jpg"
+  },
+  {
+    "revision": "31dded494b8fbf54de27850851c715c1",
+    "url": "/static/media/20.31dded49.jpg"
+  },
+  {
+    "revision": "37b7d7bcd4c212a6a319d412a2d7b7c8",
+    "url": "/static/media/21.37b7d7bc.jpg"
+  },
+  {
+    "revision": "9522354d4003471a9197b1e780bca486",
+    "url": "/static/media/22.9522354d.jpg"
+  },
+  {
+    "revision": "2234be9fb0f5956b0613a597e016be87",
+    "url": "/static/media/23.2234be9f.jpg"
+  },
+  {
+    "revision": "f48ac457b13c7e32a0f5381e83694f82",
+    "url": "/static/media/24.f48ac457.jpg"
+  },
+  {
+    "revision": "782c1c5b9f483d0a54aafccab0b313e4",
+    "url": "/static/media/25.782c1c5b.jpg"
+  },
+  {
+    "revision": "200d3a0c08b804961c42e34b9a16e94e",
+    "url": "/static/media/26.200d3a0c.jpg"
+  },
+  {
+    "revision": "d751b230e39dfbfe4295806cf9ccb8a5",
+    "url": "/static/media/27.d751b230.jpg"
+  },
+  {
+    "revision": "683b0900c8ed54bfdb28fd2593120f9e",
+    "url": "/static/media/28.683b0900.jpg"
+  },
+  {
+    "revision": "20531f1848fb4e410c5bb831346a1303",
+    "url": "/static/media/ITI-building.20531f18.jpg"
+  },
+  {
+    "revision": "902b4c80d9c398f83dc2fea3c671d8ce",
+    "url": "/static/media/ITIbuilding1.902b4c80.jpg"
+  },
+  {
+    "revision": "3650cd9b10c9c8971f2535786ff4fd1e",
+    "url": "/static/media/Parle-visit.3650cd9b.jpg"
+  },
+  {
+    "revision": "d233de111151863a4e2048320c42448e",
+    "url": "/static/media/AnjumanITI.d233de11.jpg"
+  },
+  {
+    "revision": "14d7efad667b66f9e6a33ad090ed8b15",
+    "url": "/static/media/Projector-based-study.14d7efad.jpg"
+  },
+  {
+    "revision": "eff92bd428fc4aeb199272befa55bc02",
+    "url": "/static/media/Tools.eff92bd4.jpg"
+  },
+  {
+    "revision": "617c2cfbb988a2c6266b6063b155b7af",
+    "url": "/static/media/With-students.617c2cfb.jpg"
+  },
+  {
+    "revision": "3bf89c96b48355520dae950a203d8030",
+    "url": "/static/media/With-students1.3bf89c96.jpg"
+  },
+  {
+    "revision": "daa322daf3938e55abca19538b456f8a",
+    "url": "/index.html"
+  }
+];
